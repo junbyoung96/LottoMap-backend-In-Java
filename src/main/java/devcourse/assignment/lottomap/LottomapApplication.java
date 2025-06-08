e0d039d0c05dbbf7ff99c879acb4014b11437edc
junbyoung96 <junbyoung96@gmail.com>
2025-06-08 21:20:21 +0900

    first commit

---
+++ src/main/java/devcourse/assignment/lottomap/LottomapApplication.java
@@ -0,0 +1,13 @@
+package devcourse.assignment.lottomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottomapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devcourse/assignment/lottomap/LottomapApplicationTests.java
@@ -0,0 +1,13 @@
+package devcourse.assignment.lottomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
